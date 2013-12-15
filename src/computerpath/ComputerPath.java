2f82567d594b26f3a65a13ebca41c0715f93b400
Dan <d.lanza38@gmail.com>
2013-12-15 17:22:53 -0500

    2013-12-15_computerpath with comments, completed for class

--- src/computerpath/ComputerPath.java
+++ src/computerpath/ComputerPath.java
@@ -1,14 +1,30 @@
 package computerpath;
 /**
- * @author Dan
+ * @author Daniel Lanza
+ * 
+ * Group Members:
+ * Daniel Lanza
+ * Antonio Jemmott
+ * Chanakya Chakravartula
+ * Mihir Satish Sakhardande
+ * Alldrin D'Costa
  */
 public class ComputerPath {
-    private int[][][] relation;
-    public void setRelation(int[][][] relation){
+    
+    private int[][] relation;
+    public void setRelation(int[][] relation){
         this.relation = relation;
     }    
-    public int[][][] getRelation(){
+    public int[][] getRelation(){
         return this.relation;
+    }
+    
+    private int[][] closure;
+    public void setClosure(int[][] closure){
+        this.closure = closure;
+    }    
+    public int[][] getClosure(){
+        return this.closure;
     }
     
     private int size;
@@ -19,44 +35,48 @@ public class ComputerPath {
         return this.size;
     }
     
-    
-    
+    //Process to see if there is a path of length k from node i to node j
     public int process(int i, int j, int k) {
-       
-        int path = 0;
-        
         if(k == 0){
-            if(this.relation[i][j][0] == 1){
+            if(this.relation[i][j] == 1){
+                this.closure[i][j] = 1;
                 return 1;
-            }else{ 
-                return 0;
-            }
-        }else{
-        
-            for(int step = k; step >= 0; step--){
-                for(int middle = 0; middle < this.size; middle++){
-//                    System.out.println("i = "+i+": j = "+j+": k = "+k+": middle = "+middle);
-//                    System.out.println(this.relation[i][middle][0]);
-                    if(this.relation[i][middle][0] == 1){
-                        if(this.process(middle, j, k-1) == 1){
-                            return 1;
-                        }
-                    }
-                }
-                return 0;
             }
             return 0;
         }
-    }
+
+        for(int middle = 0; middle < this.size; middle++){
+            if(this.relation[i][middle] == 1){
+                if(process(middle,j,k-1) == 1){
+                    this.closure[i][j] = 1;
+                    return 1;
+                }
+            }   
+        }
+        return 0;
+    } 
     
-    public void outputRelationSet(int k){
-        
+    //Output the relational set
+    public void outputRelationSet(){
         for (int i=0; i < this.size; i++){
             for(int j=0; j < this.size; j++){
                  if(j == this.size-1){
-                    System.out.println("'"+this.relation[i][j][k]+"'");
+                    System.out.println("'"+this.relation[i][j]+"'");
                  }else{
-                     System.out.print("'"+this.relation[i][j][k]+"'");
+                     System.out.print("'"+this.relation[i][j]+"'");
+                 }
+            }
+        }  
+    }  
+    
+    //Output the closure set
+    public void outputClosureSet(){
+        for (int i=0; i < this.size; i++){
+            for(int j=0; j < this.size; j++){
+                 if(j == this.size-1){
+                    System.out.println("'"+this.closure[i][j]+"'");
+                 }else{
+                     System.out.print("'"+this.closure[i][j]+"'");
                  }
             }
         }  
--- src/computerpath/test.java
+++ src/computerpath/test.java
@@ -1,103 +1,47 @@
 package computerpath;
 /**
- * @author Dan
+ * @author Daniel Lanza
+ * 
+ * Group Members:
+ * Daniel Lanza
+ * Antonio Jemmott
+ * Chanakya Chakravartula
+ * Mihir Satish Sakhardande
+ * Alldrin D'Costa
  */
 public class test {
     public static void main(String[] args) {
         
         int size = 4;
-        int [][][] theArray = new int[size][size][size];
+        // Primitive values are auto initialized to zero
+        int [][] theArray = new int[size][size];
         int [][] closure = new int[size][size];
         
-        for (int k=0; k < size; k++){
-            for (int i=0; i < size; i++){
-                for(int j=0; j < size; j++){
-                    theArray[i][j][k] = 0;
-                    
-                }
-            }  
-        }
+        //Turn on particular values for the initial relation
+        theArray[0][3] = 1;
+        theArray[1][2] = 1;
+        theArray[2][0] = 1;
+        theArray[3][1] = 1;
         
-        for (int i=0; i < size; i++){
-            for(int j=0; j < size; j++){
-               
-                closure[i][j] = 0;
-            }
-        }  
-
+        //Create instance of ComputerPath class
         ComputerPath thisCP = new ComputerPath();
-        thisCP.setRelation(theArray);
-
-      
-
         thisCP.setSize(size);
-
-        theArray[0][3][0] = 1;
-        theArray[1][2][0] = 1;
-        theArray[2][0][0] = 1;
-        theArray[3][1][0] = 1;
+        thisCP.setRelation(theArray);
+        thisCP.setClosure(closure);
         
-       
         System.out.println("Initial Relation");
-        thisCP.outputRelationSet(0);
+        thisCP.outputRelationSet();
         System.out.println("");
-        
+  
         for (int k=0; k < size; k++){
-            for (int i=0; i < size; i++){
-                for(int j=0; j < size; j++){
-                    
-                    theArray[i][j][k] = thisCP.process(i, j, k);
+            for (int j=0; j < size; j++){
+                for (int i=0; i < size; i++){
+                    thisCP.process(i, j, k);
                 }
             }
         }
         
-        
-        for(int k=0; k < size - 1; k++){
-            System.out.println("Set: "+k);
-            thisCP.outputRelationSet(k);
-            
-        }
-        
-        
-        int add;
-        
-        int[][][] relation;
-        
-        relation = thisCP.getRelation();
-        
-        for (int i=0; i < size; i++){
-            for(int j=0; j < size; j++){
-                add = 0;
-                for(int k=0; k < size-1; k++){
-                    
-                    if(relation[i][j][k] == 1){
-                        add = 1;
-                    }
-                }
-                if(add == 1){
-                    closure[i][j] = 1;
-                }
-            }
-        }
-        
-        System.out.println();
         System.out.println("Transitive Closure");
-        for (int i=0; i < size; i++){
-            for(int j=0; j < size; j++){
-                if(j == size-1){
-                    System.out.println("'"+closure[i][j]+"'");
-                }else{
-                    System.out.print("'"+closure[i][j]+"'");
-                }
-            }
-        }
-        
-
-        
-       
-        
-        
-        
-        
+        thisCP.outputClosureSet();
     }
 }
